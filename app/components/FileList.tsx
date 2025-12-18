d63b4addb76997fbc19faad4adbf461a8ebb9e5d
vamsimudadla <mudadlavamsi@gmail.com>
2025-12-18 18:48:19 +0530

    Added retry functionality and total status

--- app/components/FileList.tsx
+++ app/components/FileList.tsx
@@ -3,12 +3,30 @@ import { useUppy } from "../context/Uppy";
 import FileCard from "./FileCard";
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { DEFAULT_DISPLAY_LIMIT } from "../utils/constants";
+import Loader from "./common/Loader";
 
 function FileList() {
-  const { files, generateThumbnails, uppy } = useUppy();
+  const { files, generateThumbnails, uppy, state } = useUppy();
+  console.log(state);
   const [displayLimit, setDisplayLimit] = useState(() =>
     Math.min(DEFAULT_DISPLAY_LIMIT, files.length)
   );
+
+  const totalStatus = useMemo(() => {
+    const currentUploadsCount = Object.keys(state?.currentUploads || {}).length;
+    return {
+      isUploading: currentUploadsCount > 0,
+      isCompleted:
+        currentUploadsCount === 0 &&
+        state?.totalProgress === 100 &&
+        !state.error,
+      isErrored: currentUploadsCount === 0 && state?.error,
+    };
+  }, [state]);
+
+  const uploadedProgress = useMemo(() => {
+    return state?.totalProgress || 0;
+  }, [state?.totalProgress]);
 
   const displayFiles = useMemo(() => {
     return files.slice(0, displayLimit);
@@ -45,6 +63,53 @@ function FileList() {
     uppy?.upload();
   }
 
+  function retryFailed() {
+    uppy?.retryAll();
+  }
+
+  const actionButtons = useMemo(() => {
+    return totalStatus.isUploading ? (
+      <>
+        <Loader />
+        <div className="flex flex-col gap-0.5">
+          <p className="text-xs text-gray-400">Uploading...</p>
+          <p className="text-xs text-gray-400">{uploadedProgress}% of 100%</p>
+        </div>
+      </>
+    ) : totalStatus.isCompleted ? (
+      <button
+        onClick={removeAll}
+        className="bg-green-500 text-sm font-bold text-white px-6 py-1 rounded-lg cursor-pointer"
+      >
+        Done
+      </button>
+    ) : (
+      <>
+        {totalStatus.isErrored ? (
+          <button
+            onClick={retryFailed}
+            className="bg-red-500 text-sm font-bold text-white px-6 py-1 rounded-lg cursor-pointer"
+          >
+            Retry Failed
+          </button>
+        ) : (
+          <button
+            onClick={upload}
+            className="bg-orange-500 text-sm font-bold text-white px-6 py-1 rounded-lg cursor-pointer"
+          >
+            Upload
+          </button>
+        )}
+        <button
+          onClick={removeAll}
+          className="bg-white font-bold text-sm text-gray-700 px-6 py-1 rounded-lg cursor-pointer border"
+        >
+          Cancel
+        </button>
+      </>
+    );
+  }, [totalStatus]);
+
   return (
     <div className="flex flex-col flex-1">
       <div className="flex flex-col flex-1 overflow-auto p-2.5" ref={rootRef}>
@@ -56,20 +121,7 @@ function FileList() {
         {hasNextPage && <div ref={infiniteRef}></div>}
       </div>
       <div className="flex items-center justify-between h-14 shadow-2xl bg-white z-10 px-4">
-        <div className="flex items-center gap-4">
-          <button
-            onClick={upload}
-            className="bg-orange-500 text-sm font-bold text-white px-6 py-1 rounded-lg cursor-pointer"
-          >
-            Upload
-          </button>
-          <button
-            onClick={removeAll}
-            className="bg-white font-bold text-sm text-gray-700 px-6 py-1 rounded-lg cursor-pointer border"
-          >
-            Cancel
-          </button>
-        </div>
+        <div className="flex items-center gap-4">{actionButtons}</div>
       </div>
     </div>
   );
